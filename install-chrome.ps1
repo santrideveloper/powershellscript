b9d403a43c2b556a405931f84683e433996921b0
santri_developer <104056442+santrideveloper@users.noreply.github.com>
2024-09-24 10:23:08 +0700

    Update install-chrome.ps1

--- install-chrome.ps1
+++ install-chrome.ps1
@@ -1,4 +1,4 @@
-Set-Alias chromeInstall "iex (irm 'https://raw.githubusercontent.com/johndoe/my-scripts/main/install-chrome-script.ps1')"
+Set-Alias chromeInstall "iex (irm 'https://raw.githubusercontent.com/santrideveloper/powershellscript/main/install-chrome.ps1')"
 # URL untuk mengunduh Google Chrome installer
 $chromeInstallerUrl = "https://dl.google.com/chrome/install/latest/chrome_installer.exe"
 
