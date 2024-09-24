8fea303d11f3184bebbb2e3ff8a1c7bb87176018
santri_developer <104056442+santrideveloper@users.noreply.github.com>
2024-09-24 10:17:20 +0700

    Create install-chrome.ps1

---
+++ install-chrome.ps1
@@ -0,0 +1,16 @@
+# URL untuk mengunduh Google Chrome installer
+$chromeInstallerUrl = "https://dl.google.com/chrome/install/latest/chrome_installer.exe"
+
+# Lokasi penyimpanan sementara untuk installer
+$installerPath = "$env:TEMP\chrome_installer.exe"
+
+# Mengunduh installer Google Chrome
+Invoke-WebRequest -Uri $chromeInstallerUrl -OutFile $installerPath
+
+# Menjalankan installer secara diam-diam (silent install)
+Start-Process -FilePath $installerPath -Args "/silent /install" -Wait
+
+# Menghapus installer setelah instalasi selesai
+Remove-Item $installerPath
+
+Write-Host "Google Chrome telah berhasil diinstal."
